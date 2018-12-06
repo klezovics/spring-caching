8369613d23455545595b311f76acc91149874721
Arturs Klezovics <klezovich@gmail.com>
2018-12-06 10:44:31 +0000

    Name the bean

--- src/main/java/com/klezovich/spring_cache/service/implementation/UserServiceImpl.java
+++ src/main/java/com/klezovich/spring_cache/service/implementation/UserServiceImpl.java
@@ -8,7 +8,7 @@ import com.klezovich.spring_cache.entity.User;
 import com.klezovich.spring_cache.repository.UserRepository;
 import com.klezovich.spring_cache.service.UserService;
 
-@Component
+@Component("fastUserService")
 public class UserServiceImpl implements UserService {
 
 	@Autowired
