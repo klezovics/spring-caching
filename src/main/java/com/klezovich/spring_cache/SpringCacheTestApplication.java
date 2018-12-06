20048aadf293a656ca678901a94d0a6700f18bab
Arturs Klezovics <klezovich@gmail.com>
2018-12-06 10:44:24 +0000

    Add "main" method

--- src/main/java/com/klezovich/spring_cache/SpringCacheTestApplication.java
+++ src/main/java/com/klezovich/spring_cache/SpringCacheTestApplication.java
@@ -1,12 +1,27 @@
 package com.klezovich.spring_cache;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringCacheTestApplication {
+import com.klezovich.spring_cache.service.UserService;
 
+@SpringBootApplication
+public class SpringCacheTestApplication implements CommandLineRunner {
+
+	@Autowired
+	UserService userService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCacheTestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		String userName = "Arthur";
+		
+		
 	}
 }
