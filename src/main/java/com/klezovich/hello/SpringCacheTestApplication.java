f8b2f797ee83eade65a07e0a60455bf590b13154
Arturs Klezovics <klezovich@gmail.com>
2018-12-06 10:24:30 +0000

    Baseline

---
+++ src/main/java/com/klezovich/hello/SpringCacheTestApplication.java
@@ -0,0 +1,12 @@
+package com.klezovich.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCacheTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/klezovich/hello/SpringCacheTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.klezovich.hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCacheTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
